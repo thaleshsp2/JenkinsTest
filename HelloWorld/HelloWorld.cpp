de1856c8469bff7aa0ddc2987818b86fc23c84c8
Thales Henrique <thales.henrique@engenharia.ufjf.br>
2021-04-19 19:42:41 -0300

    Feat: Implement basic hello world example

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,11 @@
+//
+// Created by user on 19/04/2021.
+//
+
+#include "HelloWorld.h"
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
---
+++ HelloWorld/HelloWorld.h
@@ -0,0 +1,8 @@
+//
+// Created by user on 19/04/2021.
+//
+
+#ifndef JENKINSTEST_HELLOWORLD_H
+#define JENKINSTEST_HELLOWORLD_H
+#include <iostream>
+#endif //JENKINSTEST_HELLOWORLD_H
