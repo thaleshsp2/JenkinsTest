1ec110cbde00990001be670b9016d1f5f1a5c602
Thales Henrique <thales.henrique@engenharia.ufjf.br>
2021-04-20 18:19:34 -0300

    Feat: Implement assert usage

--- HelloWorld/src/HelloWorld.cpp
+++ HelloWorld/src/HelloWorld.cpp
@@ -4,8 +4,10 @@
 
 //#include "HelloWorld.h"
 #include <iostream>
+#include <assert.h>
 int main()
 {
     std::cout << "Cole galera, só queria dizer q o Jenkins ta rolando!" << std::endl;
+    assert(1 > 2);
     return 0;
 }
